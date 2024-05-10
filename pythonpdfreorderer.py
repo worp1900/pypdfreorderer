e043600927eedfacb0ac2e97a34427f926635c71
worp <worp@worp.io>
2024-05-10 17:38:54 +0200

    dirty functioning version

--- pythonpdfreorderer.py
+++ pythonpdfreorderer.py
@@ -20,43 +20,43 @@ readerBack = PdfReader(sys.argv[2])
 # print('Number of back pages')
 # print(len(readerFront.pages))
 
+counter = 0
+
 # add the new sequence of pages to output pdf
 for frontPageNumber in range(len(readerFront.pages)):
-    # Add the front page from the front reader
-    # writer.append(readerFront, frontPageNumber)
-    # Add the back page from the back reader in inverse order
-    # writer.append(readerBack, (len(readerFront.pages)-frontPageNumber))
-    # print(type(frontPageNumber))
-
-    # print("len(readerFront.pages)")
-    # print(len(readerFront.pages))
-    # print("range(len(readerFront.pages))")
-    # print(range(len(readerFront.pages)))
-
-    # quit()
-
+    
     backPageNumber = len(readerFront.pages)-1-frontPageNumber
 
-    print("frontPageNumber")
-    print(frontPageNumber)
-    print("backPageNumber")
-    print(backPageNumber)
-
-    # print(readerBack.pages[])
-
-    # backPageNumber = frontPageNumber-1
+    # print("frontPageNumber")
+    # print(frontPageNumber)
     # print("backPageNumber")
     # print(backPageNumber)
 
     # print("readerFront.pages[frontPageNumber]")
     # print(readerFront.pages[frontPageNumber])
-    # print("readerBack.pages[backPageNumber]")
+    # print("readerBack.pages[backPageNumber])")
     # print(readerBack.pages[backPageNumber])
-    # print("readerFront.pages[len(readerFront.pages)-frontPageNumber]")
-    # print(readerFront.pages[len(readerFront.pages)-frontPageNumber])
-    print("------")
+
+    # Add the front page from the front reader
+    writer.append(fileobj=readerFront, pages=(frontPageNumber,frontPageNumber+1))
+    # Add the back page from the back reader in inverse order
+    writer.append(fileobj=readerBack, pages=(backPageNumber,backPageNumber+1))
+    # writer.append(readerBack, readerBack.pages[backPageNumber])
+
+    # Write to an output PDF document
+    output = open(sys.argv[1]+'-mixed.pdf', "wb")
+    writer.write(output)
+
+    # if counter == 5:
+    #     break
+    # counter += 1
+
+# Close file descriptors
+writer.close()
+output.close()
+quit()
 
 # write the output pdf to file
 # outputStream = file(sys.argv[1]+'-mixed.pdf','wb')
 # writer.write(sys.argv[1]+'-mixed.pdf')
-writer.close()
+# writer.close()
