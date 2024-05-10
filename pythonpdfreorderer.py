490880d560f93cc1ccd37c35575310f09514e059
worp <worp@worp.io>
2024-05-10 17:15:29 +0200

    first draft correct iterator

--- pythonpdfreorderer.py
+++ pythonpdfreorderer.py
@@ -8,26 +8,55 @@ from pypdf import PdfWriter, PdfReader
 writer = PdfWriter()
 readerFront = PdfReader(sys.argv[1])
 readerBack = PdfReader(sys.argv[2])
-
-# page = reader.pages[0]
-# print(page.extract_text())
+# readerFront = open(sys.argv[1], "rb")
+# readerBack = open(sys.argv[2], "rb")
 
 # # construct and shuffle page number list
-pagesFront = list(range(readerFront.get_num_pages()))
-pagesBack = list(range(readerBack.get_num_pages()))
+# pagesFrontInCorrectOrder = list(range(readerFront.get_num_pages()))
+# pagesBackInCorrectOrder = list(reversed(list(range(readerBack.get_num_pages()))))
 
-print('Number of front pages')
-print(len(pagesFront))
-print('Number of back pages')
-print(len(pagesBack))
+# print('Number of front pages')
+# print(len(readerFront.pages))
+# print('Number of back pages')
+# print(len(readerFront.pages))
 
-quit()
+# add the new sequence of pages to output pdf
+for frontPageNumber in range(len(readerFront.pages)):
+    # Add the front page from the front reader
+    # writer.append(readerFront, frontPageNumber)
+    # Add the back page from the back reader in inverse order
+    # writer.append(readerBack, (len(readerFront.pages)-frontPageNumber))
+    # print(type(frontPageNumber))
 
-# # add the new sequence of pages to output pdf
-for page in pages:
-    writer.add_page(reader.get_page(page))
+    # print("len(readerFront.pages)")
+    # print(len(readerFront.pages))
+    # print("range(len(readerFront.pages))")
+    # print(range(len(readerFront.pages)))
 
-# # write the output pdf to file
+    # quit()
+
+    backPageNumber = len(readerFront.pages)-1-frontPageNumber
+
+    print("frontPageNumber")
+    print(frontPageNumber)
+    print("backPageNumber")
+    print(backPageNumber)
+
+    # print(readerBack.pages[])
+
+    # backPageNumber = frontPageNumber-1
+    # print("backPageNumber")
+    # print(backPageNumber)
+
+    # print("readerFront.pages[frontPageNumber]")
+    # print(readerFront.pages[frontPageNumber])
+    # print("readerBack.pages[backPageNumber]")
+    # print(readerBack.pages[backPageNumber])
+    # print("readerFront.pages[len(readerFront.pages)-frontPageNumber]")
+    # print(readerFront.pages[len(readerFront.pages)-frontPageNumber])
+    print("------")
+
+# write the output pdf to file
 # outputStream = file(sys.argv[1]+'-mixed.pdf','wb')
-writer.write(sys.argv[1]+'-mixed.pdf')
+# writer.write(sys.argv[1]+'-mixed.pdf')
 writer.close()
